a9441514bfaa8604518ea8d757735671113dd37e
carldkennedy <c.d.kennedy@sheffield.ac.uk>
2025-05-12 12:08:06 +0100

    Add array examples

---
+++ examples/array/pi_array_parameter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --mem=500M
+#SBATCH --job-name=pi-array-parameter
+#SBATCH --output=pi-array-parameter_%a.out
+#SBATCH --array=1-4
+
+ml hpc-examples
+
+n=$SLURM_ARRAY_TASK_ID
+iteration=`sed -n "${n} p" iterations.txt`      # Get n-th line (1-indexed) of the file
+srun python3 ${HPC_EXAMPLES}/slurm/pi.py ${iteration} > pi_iter_${iteration}.json
